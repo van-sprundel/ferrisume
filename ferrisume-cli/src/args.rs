91471ea918b795ff1daa473612d4af338d8a4a7b
van-sprundel <ramonvansprundel@gmail.com>
2024-07-31 20:59:00 +0200

    Fix clap

--- ferrisume-cli/src/args.rs
+++ ferrisume-cli/src/args.rs
@@ -1,48 +1,51 @@
-use clap::{App, Arg, ArgMatches, SubCommand};
+use clap::{Arg, ArgMatches, Command};
 
-pub fn args() -> ArgMatches<'static> {
-    App::new("ferrisume")
+pub fn args() -> ArgMatches {
+    Command::new("ferrisume")
         .version("0.0")
         .author("Ramon van Sprundel <ramonvansprundel@gmail.com>")
         .about("A resume generator CLI tool")
-        .subcommand(SubCommand::with_name("init").about("Initialize a resume.json file"))
-        .subcommand(SubCommand::with_name("watch").about("Edit your resume in a live view"))
+        .subcommand(Command::new("init").about("Initialize a resume.json file"))
+        .subcommand(Command::new("watch").about("Edit your resume in a live view"))
         .subcommand(
-            SubCommand::with_name("export")
+            Command::new("export")
                 .about("Export locally to .html or .pdf")
                 .arg(
-                    Arg::with_name("input")
-                        .short("i")
+                    Arg::new("input")
+                        .short('i')
                         .long("input")
                         .value_name("INPUT")
                         .help("Specify input file (defaults to resume.json)")
-                        .takes_value(true)
+                        .num_args(1)
+                        .required(false)
                         .default_value("resume.json"),
                 )
                 .arg(
-                    Arg::with_name("format")
-                        .short("f")
+                    Arg::new("format")
+                        .short('f')
                         .long("format")
                         .value_name("FORMAT")
                         .help("Specify output format (pdf or html)")
-                        .takes_value(true)
+                        .num_args(1)
+                        .required(false)
                         .default_value("pdf"),
                 )
                 .arg(
-                    Arg::with_name("theme")
-                        .short("t")
-                        .long("theme")
+                    Arg::new("theme")
+                        .short('t')
                         .value_name("THEME")
                         .help("Specify theme used by `export` or specify a path starting with .")
-                        .takes_value(true)
-                        .default_value("default-theme"),
+                        .num_args(1)
+                        .required(false)
+                        .default_value("default"),
                 )
                 .arg(
-                    Arg::with_name("output")
+                    Arg::new("output")
+                        .value_name("OUTPUT")
                         .help("Output file name")
                         .required(false)
                         .index(1)
-                        .default_value("resume"),
+                        .default_value("resume.pdf"),
                 ),
         )
         .get_matches()
