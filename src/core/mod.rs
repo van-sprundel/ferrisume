f469ca73780f0ee656bd3cbf0411fa481be399cf
van-sprundel <ramonvansprundel@gmail.com>
2025-05-11 14:15:41 +0200

    Add table for themes

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -3,6 +3,6 @@ pub mod resources;
 pub mod templating;
 pub mod theme;
 
+pub use export::pdf::export_to_pdf;
 pub use templating::generate_html;
 pub use theme::ThemeManager;
-pub use export::pdf::export_to_pdf;
